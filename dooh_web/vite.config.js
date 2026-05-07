34188537a6a263cf33a45fc93173d14af8c48cc5
Harry <harrywang678@gmail.com>
2026-05-07 00:16:12 -0400

    feat: pause camera and stop detection when gallery or postcard editor is open

--- dooh_web/vite.config.js
+++ dooh_web/vite.config.js
@@ -6,7 +6,7 @@ import mkcert from "vite-plugin-mkcert"
 export default defineConfig({
   plugins: [react(), mkcert()],
   optimizeDeps: {
-    include: ["onnxruntime-web"],
+    exclude: ["onnxruntime-web"],
   },
   server: {
     port: 8000,
